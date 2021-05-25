dbfb21149ad365d4b7499dfc1b1e7dfc0be16a71
Grizz03 <steve.grizz03@gmail.com>
2021-05-24 20:29:17 -0700

    added URLconf for views.py

---
+++ challenges/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+#  URL file used for storing URL's for App
+
+from . import views  # import from same file with '.'
+
+urlpatterns = [
+    path("<month>", views.month_challenge)  # Creates URLconf for views  /  accesses function and param in views.py
+]
